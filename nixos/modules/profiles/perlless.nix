85376cf907a90054b2852be30a5053ed51f36575
nikstur <nikstur@outlook.com>
2023-10-17 16:40:26 +0200

    nixos/profiles/perlless: init

---
+++ nixos/modules/profiles/perlless.nix
@@ -0,0 +1,17 @@
+{ ... }:
+
+{
+
+  # Remove perl from activation
+  boot.initrd.systemd.enable = true;
+  system.rebuildable = false;
+
+  # Perl remnants
+  system.disableInstallerTools = true;
+  programs.less.lessopen = null;
+  programs.command-not-found.enable = false;
+  boot.enableContainers = false;
+  environment.defaultPackages = [ ];
+  documentation.info.enable = false;
+
+}
