715fc90d99c3228739bb9194fa028aaffb64b143
nikstur <nikstur@outlook.com>
2023-10-17 20:28:58 +0200

    nixos/modprobe: replace activationSript
    Replace with preStart on systemd-modules-load.service.

--- nixos/modules/system/boot/modprobe.nix
+++ nixos/modules/system/boot/modprobe.nix
@@ -58,14 +58,13 @@ with lib;
 
     environment.systemPackages = [ pkgs.kmod ];
 
-    system.activationScripts.modprobe = stringAfter ["specialfs"]
-      ''
-        # Allow the kernel to find our wrapped modprobe (which searches
-        # in the right location in the Nix store for kernel modules).
-        # We need this when the kernel (or some module) auto-loads a
-        # module.
-        echo ${pkgs.kmod}/bin/modprobe > /proc/sys/kernel/modprobe
-      '';
+    systemd.services.systemd-modules-load.preStart = ''
+      # Allow the kernel to find our wrapped modprobe (which searches
+      # in the right location in the Nix store for kernel modules).
+      # We need this when the kernel (or some module) auto-loads a
+      # module.
+      echo ${pkgs.kmod}/bin/modprobe > /proc/sys/kernel/modprobe
+    '';
 
   };
 
