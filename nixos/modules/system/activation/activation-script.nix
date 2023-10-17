ccac00239ba36b50481bdb4be7dcd9a907afe4b2
nikstur <nikstur@outlook.com>
2023-10-17 17:41:29 +0200

    nixos/activation: remove specialfs script
    The stage-2-init.sh script has the same functionality hardcoded so we do
    not need it in the activationScript again.

--- nixos/modules/system/activation/activation-script.nix
+++ nixos/modules/system/activation/activation-script.nix
@@ -251,25 +251,6 @@ in
         rmdir --ignore-fail-on-non-empty /usr/bin /usr
       '';
 
-    system.activationScripts.specialfs =
-      ''
-        specialMount() {
-          local device="$1"
-          local mountPoint="$2"
-          local options="$3"
-          local fsType="$4"
-
-          if mountpoint -q "$mountPoint"; then
-            local options="remount,$options"
-          else
-            mkdir -p "$mountPoint"
-            chmod 0755 "$mountPoint"
-          fi
-          mount -t "$fsType" -o "$options" "$device" "$mountPoint"
-        }
-        source ${config.system.build.earlyMountScript}
-      '';
-
     systemd.user = {
       services.nixos-activation = {
         description = "Run user-specific NixOS activation";
