ecaa25a0b1306bf5ba502e3b0287fe6adc3c02c8
nikstur <nikstur@outlook.com>
2023-10-20 12:43:19 +0200

    nixos/envfs: remove activationScript
    These activationScript do not exist anymore and thus do not need to be
    forcibly disabled.

--- nixos/modules/tasks/filesystems/envfs.nix
+++ nixos/modules/tasks/filesystems/envfs.nix
@@ -52,9 +52,5 @@ in {
     environment.systemPackages = [ cfg.package ];
     # we also want these mounts in virtual machines.
     fileSystems = if config.virtualisation ? qemu then lib.mkVMOverride mounts else mounts;
-
-    # We no longer need those when using envfs
-    system.activationScripts.usrbinenv = lib.mkForce "";
-    system.activationScripts.binsh = lib.mkForce "";
   };
 }
