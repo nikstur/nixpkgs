98b9d23c5d22af142a64482ddbd650b3e11daaab
nikstur <nikstur@outlook.com>
2024-12-13 23:35:45 +0100

    runInLinuxVm: use sysrq to trigger shutdown

--- pkgs/build-support/vm/default.nix
+++ pkgs/build-support/vm/default.nix
@@ -208,7 +208,8 @@ rec {
 
       ${busybox}/bin/mount -o remount,ro dummy /
 
-      ${busybox}/bin/poweroff -f
+      # ${busybox}/bin/poweroff -f
+      echo "o" > /proc/sysrq-trigger
     else
       export PATH=/bin:/usr/bin:${coreutils}/bin
       echo "Starting interactive shell..."
