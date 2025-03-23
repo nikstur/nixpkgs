fc9c98b05fef1d9744803dfb9b630fae6cfdd3bd
nikstur <nikstur@outlook.com>
2025-03-23 13:38:17 +0100

    nixos/tests: test systemd state passing for nixos-init

--- nixos/tests/activation/nixos-init.nix
+++ nixos/tests/activation/nixos-init.nix
@@ -17,8 +17,6 @@
   testScript =
     { nodes, ... }: # python
     ''
-      print(machine.succeed("ls -l /run/booted-system"))
-
       with subtest("init"):
         assert "${nodes.machine.system.build.toplevel}" == machine.succeed("readlink /run/booted-system").strip()
 
@@ -26,5 +24,15 @@
         assert "${nodes.machine.system.build.toplevel}" == machine.succeed("readlink /run/current-system").strip()
         assert "${nodes.machine.hardware.firmware}/lib/firmware" == machine.succeed("cat /sys/module/firmware_class/parameters/path").strip()
         assert "${pkgs.kmod}/bin/modprobe" == machine.succeed("cat /proc/sys/kernel/modprobe").strip()
+
+      machine.wait_for_unit("multi-user.target")
+      with subtest("systemd state passing"):
+        systemd_analyze_output = machine.succeed("systemd-analyze")
+        print(systemd_analyze_output)
+        assert "(initrd)" in systemd_analyze_output, "systemd-analyze has no information about the initrd"
+
+        ps_output = machine.succeed("ps ax -o command | grep systemd | head -n 1")
+        print(ps_output)
+        assert "--deserialize" in ps_output, "--deserialize flag wasn't passed to systemd"
     '';
 }
